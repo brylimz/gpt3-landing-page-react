a7796524afef32cd3485742df309ae24324e6990
brylimz <jackjaggeriv@gmail.com>
2022-04-15 14:42:38 +0700

    stuck

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./features.css";
-const Features = () => {
-  return <div>Features</div>;
+const Features = ({title, text}) => {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+<div />
+<h1> {title} </h1>
+<div className="gpt3__features-container__feature-text" />
+{text}
+
+      </div>
+    </div>
+  );
 };
 
 export default Features;
