437e4927eda245d80a0eca13fbf4d59ff9ad4295
brylimz <jackjaggeriv@gmail.com>
2022-04-15 11:32:41 +0700

    meal time

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./import";
+
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section_padding">
+      Brand
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -27,10 +27,11 @@ const Header = () => {
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
 
+      </div>
+      
         <div className="gpt3__header-image">
           <img src={ai} alt="ai" />
         </div>
-      </div>
     </div>
   );
 };
