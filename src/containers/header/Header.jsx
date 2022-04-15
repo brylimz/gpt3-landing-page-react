504c437bdf0909d75659e2e94f9217fc85737228
brylimz <jackjaggeriv@gmail.com>
2022-04-15 10:47:09 +0700

    build css border

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -27,9 +27,9 @@ const Header = () => {
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
 
-        <div className="gpt3__header-image">
+        {/* <div className="gpt3__header-image">
           <img src={ai} alt="ai" />
-        </div>
+        </div> */}
       </div>
     </div>
   );
