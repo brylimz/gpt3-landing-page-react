5d1be507e4574ddecc4d26b6cee53aba02ceaaa1
brylimz <jackjaggeriv@gmail.com>
2022-04-18 09:51:36 +0700

    possibility section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./possibility.css";
+import possibilityImage from "../../assets/possibility.png";	
+
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div className="gpt3__possibility section__padding" id="possibility">
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -3,7 +3,7 @@ import { Feature } from "../../components";
 import "./whatGPT3.css";
 const WhatGPT3 = () => {
   return (
-    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
         <Feature
           title="What is GPT 3"
